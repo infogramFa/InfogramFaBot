62f3c5a6b76f3d3ee65a001afc0b7ec26abdd2b2
mzarchi <me@zartweety.ir>
2023-12-15 17:25:44 +0330

    Add bot sample config

---
+++ config-example.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'system/Telegram.php';
+require_once 'system/Database.php';
+require_once 'system/Core.php';
+
+const _TOKEN = "bot-token";
+
+const _TimeStamp = "1605643011";
+
+global $config;
+$config['host'] = "localhost";
+$config['user'] = "username";
+$config['pass'] = "password";
+$config['name'] = "database-name";
